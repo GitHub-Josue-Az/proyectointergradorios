e956fdbac14f06e47d0a7a189f158a4e344b1c18
GitHub-Josue-Az <josue.alva@tecsup.edu.pe>
2019-06-18 19:45:59 -0500

    Initial Commit

---
+++ ProyectoIntegradorAdmin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProyectoIntegradorAdmin
+//
+//  Created by JosueAlva on 18/06/19.
+//  Copyright © 2019 JosueAlva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
